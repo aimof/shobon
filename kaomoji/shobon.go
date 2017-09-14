db15493fd743db84b7e26cf853f69a4ef7863057
aimof <aimof.aimof@gmail.com>
2017-09-15 02:05:33 +0900

    almost complete ver0

--- kaomoji/shobon.go
+++ kaomoji/shobon.go
@@ -1,5 +1,15 @@
 package kaomoji
 
 const (
-	SHOBON = "  .0^  .Y^                                                     (Tp `?4+. 	\n gF                                                                   dN. \n(M:                (MMN.        (H=   ?We.        dMMR                 Mb 	\ndN                 ?MMD        (#: .(,  Mb        7MMY                 dN 	\ndN                             M#_ .M|  d#                             d# \ndN.                            ?Me..Mn..MF                            .M@ \n.ML                            `.7'!`-7''/                            (#' \n  Te.                                                               .(9!\n"
+	SHOBON        = "  .0^  .Y^                                                     (Tp `?4+. 	\n gF                                                                   dN. \n(M:                (MMN.        (H=   ?We.        dMMR                 Mb 	\ndN                 ?MMD        (#: .(,  Mb        7MMY                 dN 	\ndN                             M#_ .M|  d#                             d# \ndN.                            ?Me..Mn..MF                            .M@ \n.ML                            `.7'!`-7''/                            (#' \n  Te.                                                               .(9!\n"
+	SHOBON0       = "  .0^  .Y^                                                     (Tp `?4+"
+	SHOBON1       = " gF                                   .                               dN."
+	SHOBON2       = "(M:                (MMN.        (H=   ?We.        dMMR                 Mb"
+	SHOBON3       = "dN                 ?MMD        (#: .(,  Mb        7MMY                 dN"
+	SHOBON4       = "dN                             M#_ .M|  d#                             d#"
+	SHOBON5       = "dN.                            ?Me..Mn..MF                            .M@"
+	SHOBON6       = " .ML                            `.7'!`-7'!                            (#'"
+	SHOBON7       = "  Te.                                                               .(9!"
+	SHOBON_WIDTH  = 74
+	SHOBON_HEIGHT = 8
 )
--- main.go
+++ main.go
@@ -3,8 +3,61 @@ package main
 import (
 	"fmt"
 	"github.com/aimof/shobon/kaomoji"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println(kaomoji.SHOBON)
+	var out io.Writer = os.Stdout
+	fmt.Fprint(out, "\x1b[2J")
+	cmd := exec.Command("stty", "size")
+	cmd.Stdin = os.Stdin
+	rawSize, err := cmd.Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	size := strings.Split(string(rawSize), " ")
+	height, err := strconv.Atoi(size[0])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	width, err := strconv.Atoi(strings.Split(size[1], "\n")[0])
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if height < 8 {
+		fmt.Fprintln(out, "\x1b[2J")
+		fmt.Print(kaomoji.SHOBON0 + "\n")
+		fmt.Print(kaomoji.SHOBON1 + "\n")
+		fmt.Print(kaomoji.SHOBON2 + "\n")
+		fmt.Print(kaomoji.SHOBON3 + "\n")
+		fmt.Print(kaomoji.SHOBON4 + "\n")
+		fmt.Print(kaomoji.SHOBON5 + "\n")
+		fmt.Print(kaomoji.SHOBON6 + "\n")
+		fmt.Print(kaomoji.SHOBON7 + "\n")
+	} else {
+		for i := 0; i < height-8; i++ {
+			fmt.Fprintln(out, "\x1b[2J")
+			fmt.Print(strings.Repeat("\n", i))
+			whiteSpace := ""
+			if width > 74 {
+				whiteSpace = strings.Repeat(" ", (width-74)/2)
+			}
+			fmt.Print(whiteSpace + kaomoji.SHOBON0 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON1 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON2 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON3 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON4 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON5 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON6 + "\n")
+			fmt.Print(whiteSpace + kaomoji.SHOBON7 + "\n")
+			fmt.Print(strings.Repeat("\n", height-i-kaomoji.SHOBON_HEIGHT))
+			time.Sleep(150 * time.Millisecond)
+		}
+	}
 }
