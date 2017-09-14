645f82d90aea0ec8a6496a3399ea2891b547b6a7
aimof <aimof.aimof@gmail.com>
2017-09-15 00:59:45 +0900

    initial commit

---
+++ kaomoji/shobon.go
@@ -0,0 +1,5 @@
+package kaomoji
+
+const (
+	SHOBON = "  .0^  .Y^                                                     (Tp `?4+. 	\n gF                                                                   dN. \n(M:                (MMN.        (H=   ?We.        dMMR                 Mb 	\ndN                 ?MMD        (#: .(,  Mb        7MMY                 dN 	\ndN                             M#_ .M|  d#                             d# \ndN.                            ?Me..Mn..MF                            .M@ \n.ML                            `.7'!`-7''/                            (#' \n  Te.                                                               .(9!\n"
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aimof/shobon/kaomoji"
+)
+
+func main() {
+	fmt.Println(kaomoji.SHOBON)
+}
